b45bcd2b8e3a7d781596b67a51a1a2ea8a4617e4
“reshma” <shaikreshma1508@gmail.com>
2025-11-28 12:17:15 +0530

    modified the index.js,db.cong.js and added docker files in both frontend and backend and dockercompose.yml

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -5,7 +5,18 @@ mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = dbConfig.url; // <- now reads MONGO_URL if set
 db.tutorials = require("./tutorial.model.js")(mongoose);
 
 module.exports = db;
+// const dbConfig = require("../config/db.config.js");
+
+// const mongoose = require("mongoose");
+// mongoose.Promise = global.Promise;
+
+// const db = {};
+// db.mongoose = mongoose;
+// db.url = dbConfig.url;
+// db.tutorials = require("./tutorial.model.js")(mongoose);
+
+// module.exports = db;
